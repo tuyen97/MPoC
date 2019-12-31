c3ad6dcc39f1570976bddb284d8898daf47cf9e8
bkc_3 <nguyentrongtuyen15197@gmail.com>
2019-12-31 15:56:26 +0700

    split docker  compose

--- gen.py
+++ gen.py
@@ -4,25 +4,25 @@ import json
 numnode = sys.argv[1]
 
 
-def genDockerCompose():
-    w = open("docker-compose.yml", "w")
+def genDockerCompose(start_from):
+    w = open("docker-compose-{}.yml".format(start_from), "w")
     w.write("version: '3'\n")
     w.write("services:\n")
     for i in range(int(numnode)):
-        w.write("  node_"+str(i)+":\n")
+        w.write("  node_"+str(i+int(start_from))+":\n")
         # w.write("    image: mdpos\n")
         w.write("    build:\n")
         w.write("      context: .\n")
         w.write("      dockerfile: ./Dockerfile\n")
         w.write("    network_mode: host\n")
         w.write("    ports:\n")
-        w.write("      - "+str(8000+i)+":8000\n")
-        w.write("    command: ./mdpos {} {} {}\n".format(8000+i,9000+i,i))
-        w.write("    hostname: node_"+str(i)+"\n")
+        w.write("      - "+str(8000+i+int(start_from))+":8000\n")
+        w.write("    command: ./mdpos {} {} {}\n".format(8000+i+int(start_from),9000+i+int(start_from),i+int(start_from)))
+        w.write("    hostname: node_"+str(i+int(start_from))+"\n")
         # w.write("\t\tnetwork_mode: \"host\"\n")
     w.close()
 
 
 if __name__ == "__main__":
-    genDockerCompose()
+    genDockerCompose(sys.argv[2])
     # startcluster()
