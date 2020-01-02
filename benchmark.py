681063ad8d038251775f5898c856b9c7c2633f2b
tuyen97 <nguyentrongtuyen15197@gmail.com>
2020-01-03 01:22:48 +0700

    change benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import random
 import queue
@@ -14,11 +15,12 @@ def benchmark(item):
 
 
 if __name__ == "__main__":
+    wrk_folder = sys.argv[1]
     # command=['pwd', 'pwd', 'pwd']
     # print(command)
     path_wrk = '/home/bkc_3/Desktop/wrk/wrk'
     destination_ip = '127.0.0.1'
-    
+
     start_time = time.time()
     print(start_time)
     command_round = []
@@ -27,11 +29,11 @@ if __name__ == "__main__":
         for j in range(70):
             destimation_port = 8000 + j
             time_benchwrk = random.choice(range(20))
-            _command = ' -t12 -c400 -d{}s -s /home/bkc_3/Desktop/wrk/scripts/post_lua/post_{}.lua  http://{}:{}'\
-                .format(time_benchwrk, j, destination_ip, destimation_port)
+            _command = ' -t12 -c400 -d{}s -s {}/post_{}.lua  http://{}:{}'\
+                .format(time_benchwrk, wrk_folder, j, destination_ip, destimation_port)
             _command_round.append(_command)
         command_round.append(_command_round)
-    
+
     for i in range(10):
         param_grid = {
             'command': command_round[i]
