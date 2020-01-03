42feb7a07ceb76376d73fcab4e0ca4d29aee48c4
tuyen97 <nguyentrongtuyen15197@gmail.com>
2020-01-03 22:54:09 +0700

    gen docker compose

--- gen.py
+++ gen.py
@@ -1,28 +1,38 @@
 import sys
 import json
 
-numnode = sys.argv[1]
+numnode = int(sys.argv[1])
 
 
-def genDockerCompose(start_from):
-    w = open("docker-compose-{}.yml".format(start_from), "w")
-    w.write("version: '3'\n")
-    w.write("services:\n")
-    for i in range(int(numnode)):
-        w.write("  node_"+str(i+int(start_from))+":\n")
-        # w.write("    image: mdpos\n")
-        w.write("    build:\n")
-        w.write("      context: .\n")
-        w.write("      dockerfile: ./Dockerfile\n")
-        w.write("    network_mode: host\n")
-        w.write("    ports:\n")
-        w.write("      - "+str(8000+i+int(start_from))+":8000\n")
-        w.write("    command: ./mdpos {} {} {}\n".format(8000+i+int(start_from),9000+i+int(start_from),i+int(start_from)))
-        w.write("    hostname: node_"+str(i+int(start_from))+"\n")
-        # w.write("\t\tnetwork_mode: \"host\"\n")
-    w.close()
+def genDockerCompose():
+    perFile = 50
+    numFile = int(numnode/50)
+    for i in range(numFile):
+        w = open("docker-compose-{}.yml".format(i), "w")
+        w.write("version: '3'\n")
+        w.write("services:\n")
+        if i == 0:
+            w.write("  dns:\n")
+            w.write("    image: mdpos\n")
+            w.write("    network_mode: host\n")
+            w.write("    command: ./mdpos 1\n")
+        for j in range(i*50, min((i+1)*50, numnode)):
+            # print(j)
+            w.write("  node_"+str(j)+":\n")
+            # w.write("    image: mdpos\n")
+            # w.write("    build:\n")
+            # w.write("      context: .\n")
+            # w.write("      dockerfile: ./Dockerfile\n")
+            w.write("    image: mdpos\n")
+            w.write("    network_mode: host\n")
+            w.write("    ports:\n")
+            w.write("      - "+str(8000+j)+":8000\n")
+            w.write("    command: ./mdpos {} {} {}\n".format(8000+j, 9000+j, j))
+            w.write("    hostname: node_"+str(j)+"\n")
+            # w.write("\t\tnetwork_mode: \"host\"\n")
+        w.close()
 
 
 if __name__ == "__main__":
-    genDockerCompose(sys.argv[2])
+    genDockerCompose()
     # startcluster()
